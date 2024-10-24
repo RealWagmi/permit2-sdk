3093609950287d74eecebbe18246b36fad47fa07
Alex <tarkovskiy.alexandr@protonmail.com>
2024-10-24 10:55:09 +0300

    added sdk

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# permit2-sdk
+# Permit2 sdk
+
+[![npm version](https://img.shields.io/npm/v/@real-wagmi/permit2-sdk/latest.svg)](https://www.npmjs.com/package/@real-wagmi/permit2-sdk/v/latest)
---
+++ src/constants.ts
@@ -0,0 +1,48 @@
+import { ChainId } from '@real-wagmi/sdk';
+import { Address } from 'viem';
+
+// @fixme convert to ChainId after all chains are updated
+const PERMIT2_ADDRESSES: Record<ChainId, Address | undefined> = {
+    [ChainId.ETHEREUM]: '0x4b5d9db7910448e2F236509D9eE242673AFa28aA',
+    [ChainId.BASE]: '0x16b413526d062142eb9eDA9d75312cA9f2e418FD',
+    [ChainId.ZKSYNC]: '0x87C0878B54c174199f438470FD74B3F7e1Def295',
+    [ChainId.FANTOM]: '0x07Ed33a242BD9C08CA3C198e01189e35265024Da',
+    [ChainId.ARBITRUM]: '0xD6EA74e1b6149195999c820706Ee249E7028Dc55',
+    [ChainId.KAVA]: '0xB5fa77E3929fe198a86Aa40fd6c77886785bCd0e',
+    [ChainId.OPTIMISM]: '0x127d30002C2aeDc64A5e0C4Ee2043105F58107a9',
+    [ChainId.METIS]: '0xb1F795776cB9DdAC6E7e162f31C7419Dd3d48297',
+    [ChainId.METIS_SEPOLIA]: '0x1Ac569879EF7EacB17CC373EF801cDcE4acCdeD5',
+    [ChainId.ZKLINK]: '0x8C685427ac9bB5D7915A2EC165F3aD9B1404D1A9',
+    [ChainId.IOTA]: '0x8d8B490fCe6Ca1A31752E7cFAFa954Bf30eB7EE2',
+    [ChainId.AVALANCHE]: '0x037F7b8de425Afc8A544920Bfb0786c1Fd74E473',
+    [ChainId.POLYGON]: '0x5c811Ce03C3C56790C1cF2f3068713b80AB59495',
+    [ChainId.BSC]: '0x678F6b93FB0670Be8a6610795cA4896Dc217a650',
+    [ChainId.BLAST]: undefined,
+    [ChainId.SONIC_TESTNET]: '0x1Ac569879EF7EacB17CC373EF801cDcE4acCdeD5',
+};
+
+export const getPermit2Address = (chainId: ChainId): Address => {
+    const address = PERMIT2_ADDRESSES[chainId];
+    if (!address) throw new Error(`Permit2 not deployed on chain ${chainId}`);
+    return address;
+};
+
+export const MaxUint48 = BigInt('0xffffffffffff');
+export const MaxUint160 = BigInt('0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF');
+export const MaxUint256 = BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
+
+// alias max types for their usages
+// allowance transfer types
+export const MaxAllowanceTransferAmount = MaxUint160;
+export const MaxAllowanceExpiration = MaxUint48;
+export const MaxOrderedNonce = MaxUint48;
+
+// signature transfer types
+export const MaxSignatureTransferAmount = MaxUint256;
+export const MaxUnorderedNonce = MaxUint256;
+export const MaxSigDeadline = MaxUint256;
+
+export const PERMIT_EXPIRATION = 2592000000; // 30 day
+export const PERMIT_SIG_EXPIRATION = 1800000; // 30 min
+
+export const InstantExpiration = BigInt('0');
